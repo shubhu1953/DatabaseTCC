5acba656d1204828df7d614de02dc8b8a60bbc41
shubhu1953 <shubhu1980@gmail.com>
2017-08-19 18:03:44 +0100

    modified

---
+++ Scripts/deploy.ps1
@@ -0,0 +1,11 @@
+﻿#--TeamCity checkout directory command line argument--
+		param([string]$tcDir = "")
+                #--Load the Dac.dll into the PowerShell session--
+		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
+                #--Load the SQL Server instance--
+		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=(SHUBHANKAR\SQLEXPRESS)"
+                #--Load the DACPAC--
+		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\DatabaseTCC\DatabaseTCC\bin\Release\DatabaseTCC.dacpac")
+                #--Deploy your database--
+		$dacService.deploy($dacPac, "QA", "True") 
+		
