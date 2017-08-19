8cd33afcfec5e46162cf7eb9e601382f0919e5ff
shubhu1953 <shubhu1980@gmail.com>
2017-08-19 22:02:15 +0100

    powershell

---
+++ Scripts/Untitled2.ps1
@@ -0,0 +1,23 @@
+﻿$Server = '.\SQLEXPRESS'    
+$database = 'Development'
+$Connection = New-Object System.Data.SQLClient.SQLConnection
+$Connection.ConnectionString = "server=$($Server);database=$($Database);Integrated Security = True;"
+$Connection.Open()
+$Command = New-Object System.Data.SQLClient.SQLCommand
+$Command.Connection = $Connection
+$Command.CommandText = 'Select * from dbo.Employees'
+try{                                                                                                                                               
+$Reader = $Command.ExecuteReader()
+}
+
+catch 
+{ 
+Write-Host "LoadException"; 
+$Error | format-list -force 
+Write-Host $Error[0].Exception.ParentContainsErrorRecordException; 
+}
+
+$Datatable = New-Object System.Data.DataTable
+$Datatable.Load($Reader)
+$Datatable
+$Connection.Close() 
--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -1,11 +1,29 @@
-﻿#--TeamCity checkout directory command line argument--
+﻿
+
+
+#--TeamCity checkout directory command line argument--
 		param([string]$tcDir = "")
                 #--Load the Dac.dll into the PowerShell session--
-		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\130\DAC\bin\Microsoft.SqlServer.Dac.dll"
+        $Server = '.\SQLEXPRESS'    
+        $database = 'Development'
+		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
                 #--Load the SQL Server instance--
-		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=(SHUBHANKAR\SQLEXPRESS)"
+		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=$($Server);Integrated Security = True;"
                 #--Load the DACPAC--
 		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\bin\Release\DatabaseTCC.dacpac")
                 #--Deploy your database--
-		$dacService.deploy($dacPac, "QA", "True") 
+		
+try 
+{ 
+# Deploy the dacpac 
+$dacService.deploy($dacPac, "QA", "TRUE") 
+} 
+catch 
+{ 
+Write-Host "LoadException"; 
+$Error | format-list -force 
+Write-Host $Error[0].Exception.ParentContainsErrorRecordException; 
+}
+
+
 		
