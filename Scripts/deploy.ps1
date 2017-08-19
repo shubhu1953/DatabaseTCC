586c326a4bdf06f304e377a981d815570f7ee067
shubhu1953 <shubhu1980@gmail.com>
2017-08-19 22:24:54 +0100

    j

--- Scripts/deploy.ps1
+++ Scripts/deploy.ps1
@@ -6,9 +6,10 @@
                 #--Load the Dac.dll into the PowerShell session--
         $Server = '.\SQLEXPRESS'    
         $database = 'Development'
+        $userId = 'Shubhankar\Joy'
 		Add-Type -Path "C:\Program Files (x86)\Microsoft SQL Server\120\DAC\bin\Microsoft.SqlServer.Dac.dll"
                 #--Load the SQL Server instance--
-		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=$($Server);Integrated Security = True;"
+		$dacService = new-object Microsoft.SqlServer.Dac.DacServices "server=$($Server);integrated Security = True;"
                 #--Load the DACPAC--
 		$dacPac = [Microsoft.SqlServer.Dac.DacPackage]::Load("$tcDir\bin\Release\DatabaseTCC.dacpac")
                 #--Deploy your database--
