99c9701744e5418fdef81209523beb0b702be85a
shubhu1953 <shubhu1980@gmail.com>
2017-08-19 22:34:15 +0100

    orders

---
+++ Script.PostDeployment1.sql
@@ -0,0 +1,22 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+IF (EXISTS(SELECT * FROM [dbo].[Employees]))
+BEGIN
+	DELETE FROM [dbo].[Employees]
+END
+
+INSERT INTO [dbo].Employees VALUES ('Warren', 'Buffet', 'CFO')
+INSERT INTO [dbo].Employees VALUES ('Bill', 'Gates', 'CEO')
+INSERT INTO [dbo].Employees VALUES ('Oprah', 'Winfrey', 'CEO')
+INSERT INTO [dbo].Employees VALUES ('Steve', 'Jobs', 'CTO')
+INSERT INTO [dbo].Employees VALUES ('Mark', 'Zuckerberg', 'Co-Founder')
+INSERT INTO [dbo].Employees VALUES ('Percy', 'Miller', 'Mogul')
---
+++ dbo/Tables/Orders.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderId]    INT IDENTITY (1, 1) NOT NULL,
+    [EmployeeId] INT NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([OrderId] ASC),
+    CONSTRAINT [FK_Orders_Employees] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employees] ([EmployeeId])
+);
